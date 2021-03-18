577742ddb0fdb731fb9aeb5aec189a153ad69a26
gavin <gavin.heidenreich@gmail.com>
2021-03-18 11:33:36 -0400

    broken commit 2

--- organize_data.py
+++ organize_data.py
@@ -2,17 +2,17 @@ import os
 import shutil
 
 # this script organizes data into proper dir structure for training
+# should add a shuffle, but it's pretty random as is
 
 train_set_ratio = 0.8
 val_set_ratio = 0.1
 test_set_ratio = 0.1
 
-os.makedirs('./data/tr/1', exist_ok=True)
-os.makedirs('./data/tr/0', exist_ok=True)
-os.makedirs('./data/v/1', exist_ok=True)
-os.makedirs('./data/v/0', exist_ok=True)
-os.makedirs('./data/te/1', exist_ok=True)
-os.makedirs('./data/te/0', exist_ok=True)
+os.makedirs('./data/train/1', exist_ok=True)
+os.makedirs('./data/train/0', exist_ok=True)
+os.makedirs('./data/valid/1', exist_ok=True)
+os.makedirs('./data/valid/0', exist_ok=True)
+os.makedirs('./data/test/predict', exist_ok=True)
 
 train_set_size = round(len(os.listdir('./train_dir/1')) * train_set_ratio)
 val_set_size = round(len(os.listdir('./train_dir/1')) * val_set_ratio)
@@ -31,7 +31,8 @@ val_0_fn = zeroes[train_set_size+test_set_size:]
 
 fns = [train_1_fn, test_1_fn, val_1_fn, train_0_fn, test_0_fn, val_0_fn]
 bases = ['./train_dir/1/', './train_dir/1/', './train_dir/1/', './train_dir/0/', './train_dir/0/', './train_dir/0/']
-new_dirs = ['./data/tr/1', './data/te/1', './data/v/1', './data/tr/0', './data/te/0', './data/v/0']
+new_dirs = ['./data/train/1', './data/test/predict', './data/valid/1', './data/train/0', './data/test/predict', './data/valid/0']
+
 for i in range(6):
     for fn in fns[i]:
         shutil.copyfile(bases[i] + fn, new_dirs[i] + os.sep + fn)
