603c4aa57fc8cdbbf10494019196dede3be7859e
gavin <gavin.heidenreich@gmail.com>
2021-03-19 11:50:28 -0400

    tweaked model

--- slide_detector.py
+++ slide_detector.py
@@ -2,7 +2,7 @@ from warnings import simplefilter
 simplefilter(action='ignore', category=FutureWarning)
 simplefilter(action='ignore', category=UserWarning)
 
-from tensorflow.keras.applications import EfficientNetB0 as Net
+from tensorflow.keras.applications import EfficientNetB3 as Net
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers.experimental import preprocessing
 from tensorflow.keras import models
@@ -16,6 +16,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import time
 
 import cv2
 
@@ -23,11 +24,11 @@ train_dir = '/home/gavin/offline_analysis/data/train/'
 validation_dir = '/home/gavin/offline_analysis/data/valid/'
 test_dir = '/home/gavin/offline_analysis/data/test/'
 
-batch_size = 3
+batch_size = 16
 
 width = 224
 height = 224
-epochs = 150
+epochs = 200
 NUM_TRAIN = len(os.listdir(train_dir + '1')) + len(os.listdir(train_dir + '0'))
 NUM_TEST = len(os.listdir(test_dir + 'predict'))
 NUM_VAL = len(os.listdir(validation_dir + '1')) + len(os.listdir(validation_dir + '0'))
@@ -36,16 +37,16 @@ print(NUM_TRAIN, NUM_TEST, NUM_VAL)
 
 dropout_rate = 0.2
 # dropout_rate = 0
-input_shape = (height, width, 1)
+input_shape = (height, width, 3)
 
-
+#cant really do much augmentation cause the pellet is right around the edge of the frame in some frames
 train_datagen = ImageDataGenerator(
-      rescale=1./255,
-      rotation_range=40,
-      width_shift_range=0.2,
-      height_shift_range=0.2,
-      shear_range=0.2,
-      zoom_range=0.2,
+      # rescale=1./255,
+      rotation_range=10,
+      #width_shift_range=0.2,
+      #height_shift_range=0.2,
+      #shear_range=0.2,
+      #zoom_range=0.2,
       horizontal_flip=True,
       fill_mode='nearest')
 
@@ -61,7 +62,7 @@ train_generator = train_datagen.flow_from_directory(
         class_mode='binary')
 
 # Note that the validation data should not be augmented!
-test_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator()#rescale=1./255)
 
 validation_generator = test_datagen.flow_from_directory(
         directory=validation_dir,
@@ -81,33 +82,6 @@ test_generator = test_datagen.flow_from_directory(
     seed=42
 )
 
-
-def model1():
-    conv_base = Net(weights='imagenet', include_top=False, input_shape=input_shape)
-
-    model = models.Sequential()
-    model.add(conv_base)
-    model.add(layers.GlobalMaxPooling2D(name="gap"))
-    # model.add(layers.Flatten(name="flatten"))
-    if dropout_rate > 0:
-        model.add(layers.Dropout(dropout_rate, name="dropout_out"))
-    # model.add(layers.Dense(256, activation='relu', name="fc1"))
-    # model.add(layers.Dense(2, activation='softmax', name="fc_out"))
-    model.add(layers.Dense(2, activation='softmax', name="fc_out"))
-
-
-    model.summary()
-
-    conv_base.trainable = False
-
-
-    model.compile(loss='categorical_crossentropy',
-                optimizer=optimizers.RMSprop(lr=2e-1),
-                metrics=['acc'])
-
-    return model
-
-
 def model2():
     efficient_net = Net(
         weights='imagenet',
@@ -118,8 +92,7 @@ def model2():
 
     model = models.Sequential()
     model.add(efficient_net)
-    #model.add(layers.Dense(units = 120, activation='relu'))
-    model.add(layers.Dropout(0.2, name="dropout_out"))
+    model.add(layers.Dropout(dropout_rate, name='dropout_layer1'))
     model.add(layers.Dense(units = 64, activation = 'relu'))
     model.add(layers.Dense(units = 32, activation = 'relu'))
     model.add(layers.Dense(units = 16, activation = 'relu'))
@@ -128,30 +101,8 @@ def model2():
 
     efficient_net.trainable = False
 
-    model.compile(optimizer=optimizers.Adam(lr=0.000001), loss='binary_crossentropy', metrics=['binary_accuracy'])
+    model.compile(optimizer=optimizers.Adam(lr=0.0001), loss='binary_crossentropy', metrics=['binary_accuracy'])
     return model
-
-def plot1(history):
-    acc = history.history['acc']
-    val_acc = history.history['val_acc']
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-
-    epochs_x = range(len(acc))
-
-    plt.plot(epochs_x, acc, 'bo', label='Training acc')
-    plt.plot(epochs_x, val_acc, 'b', label='Validation acc')
-    plt.title('Training and validation accuracy')
-    plt.legend()
-
-    plt.figure()
-
-    plt.plot(epochs_x, loss, 'bo', label='Training loss')
-    plt.plot(epochs_x, val_loss, 'b', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.legend()
-
-    plt.show()
 
 def plot2(history):
     acc = history.history['binary_accuracy']
@@ -193,19 +144,35 @@ if __name__ == '__main__':
 
     plot2(history)
         
-    pred = model.predict_generator(test_generator)
-    print(pred)
-    print(type(pred))
+    # pred = model.predict_generator(test_generator)
+    # print(pred)
+    # print(type(pred))
 
-    # model.evaluate_generator(generator=validation_generator, steps=1, verbose=1)
-    # test_generator.reset()
-    # pred = model.predict_generator(test_generator, steps=1, verbose=1)
+    # score = model.evaluate_generator(generator=validation_generator, steps=1, verbose=1)
+    # print(score)
+    # pred = model.predict_generator(test_generator)[:10]
+    # print(pred)
+    # res = list(zip(test_generator.filenames[:10], pred))
+    # print(res)
+    # print(res[0])
+    # print(res[1])
+    # for r in res:
+    #     fn, pred = r[0], str(r[1][0])
+    #     fn = test_dir + fn
+    #     print(fn)
+    #     img = cv2.imread(fn)
+    #     # img = cv2.putText(img, pred, (20,20), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0))
+    #     cv2.imshow('test', img)
+    #     time.sleep(12)
     # predicted_class_indices = np.argmax(pred, axis=1)
+    # print(f'predicted_class_indices: {predicted_class_indices}')
     # labels = (train_generator.class_indices)
-    # labels = dict((v, k) for v, k in labels.items())
+    # print(f'labels: {labels}')
+    # labels = dict((k, v) for v, k in labels.items())
     # predictions = [labels[k] for k in predicted_class_indices]
 
     # filenames=test_generator.filenames
     # results=pd.DataFrame({"Filename":filenames, "Predictions":predictions})
+    # print(results)
     # results.to_csv("results.csv",index=False)
     # plot1(history)
